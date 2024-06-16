0537af5d751cffbd9943b48f32e2a4dd554fae67
anscaryo <anscaryo@gmail.com>
2024-06-16 09:42:51 +0200

    Añado configuracion de picom en la carpeta script/picom y condicional para ejecutar resolución de pantalla

--- script/autostart.sh
+++ script/autostart.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 setxkbmap -layout es &
 # /home/ansgair/display_conf1.sh &
-picom & # --config $HOME/.config/qtile/scripts/picom/picom.comf &
+pgrep -x picom > /dev/null || picom  --config $HOME/.config/qtile/script/picom/picom.conf &
+
 #feh --bg-fill $HOME/.config/qtile/wallpaper/calavera.jpg -Z $HOME/.config/qtile/wallpaper/command.png &
 nitrogen --restore &
 #   Iconos de sistema.
@@ -10,7 +11,13 @@ nm-applet &
 volumeicon &
 cbatticon -u 5 &
 #screenout.sh &
-xrandr --output Virtual1  --mode 1920x1080+0+0 &
-xrandr --output Virtual2  --mode 1920x1080 --rotate left &
+
+if [ -f $HOME/.screenlayout/screenlayout.sh ]
+then
+  $HOME/.screenlayout/screenlayout.sh &
+fi
+
+#xrandr --output Virtual1  --mode 1920x1080+0+0 &
+#xrandr --output Virtual2  --mode 1920x1080 --rotate left &
 #conky &
 (conky -c $HOME/.config/qtile/script/conky/conky.conf) &

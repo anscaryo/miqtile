04b7fc88b636e97e1961bff5474cbd8a62cb82ab
anscaryo <anscaryo@gmail.com>
2024-06-25 20:14:39 +0200

    Añado xfce4-nofifyc y blueberry-tray al autoarranque de qtile, reordeno el archivo

--- script/autostart.sh
+++ script/autostart.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 setxkbmap -layout es &
 # /home/ansgair/display_conf1.sh &
-pgrep -x picom > /dev/null || picom  --config $HOME/.config/qtile/script/picom/picom.conf &
-
-#feh --bg-fill $HOME/.config/qtile/wallpaper/calavera.jpg -Z $HOME/.config/qtile/wallpaper/command.png &
-nitrogen --restore &
 #   Iconos de sistema.
 udiskie -t &
 nm-applet &
 volumeicon &
-cbatticon -u 5 &
+cbatticon -u 5 -p &
 #screenout.sh &
+blueberry-tray &
+
+# --->  Configuracion del escritorio  <---
 
 if [ -f $HOME/.screenlayout/screenlayout.sh ]
 then
   $HOME/.screenlayout/screenlayout.sh &
 fi
+pgrep -x picom > /dev/null || picom  --config $HOME/.config/qtile/script/picom/picom.conf &
+nitrogen --restore &
+#feh --bg-fill $HOME/.config/qtile/wallpaper/calavera.jpg -Z $HOME/.config/qtile/wallpaper/command.png &
 
-#xrandr --output Virtual1  --mode 1920x1080+0+0 &
-#xrandr --output Virtual2  --mode 1920x1080 --rotate left &
-#conky &
 pgrep -x conky || (conky -c $HOME/.config/qtile/script/conky/conky.conf) &
 
+
+# --->  Configuracion de sistema  <---
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
